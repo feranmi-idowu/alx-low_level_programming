57f861839616ab62d9e24e9a7ad7f23553023b34
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-07 07:08:57 -0700

    my personal work

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 /*
  * generate rondom numbers
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is negative\n", rand());
+printf("%d is negative\n", n);
 }
 else if (n == 0)
 {
-printf("%d is zero\n", rand());
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%d is positive\n", rand());
+printf("%d is positive\n", n);
 }
-return 0;
+return (0);
 }
