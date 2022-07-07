f5e077efe807d154712b29fcac15ef06a2cce24e
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-07 15:55:01 -0700

    my personal work

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,10 @@ int main(void)
 {
 char a;
 for (a = 'a'; a <= 'z'; a++)
+{
+if (a != 'q' && a != 'e')
 putchar (a);
-{
-if (a == 'q' || a == 'e')
-{
-continue;
 }
-putchar (a);
+putchar ('\n');
 return (0);
-
 }
