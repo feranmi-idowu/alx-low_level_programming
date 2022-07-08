83b26cc6621d166513580b504cf4818ee7479b66
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-08 01:25:37 -0700

    my personal work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,16 @@ for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
+if ((digit2 % 10) > (digit1 % 10))
+{
+putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
-continue;
+if (digit1 != 18 || digit2 != 19)
+{
 putchar(',');
 putchar(' ');
+}
+}
 }
 }
 putchar('\n');
