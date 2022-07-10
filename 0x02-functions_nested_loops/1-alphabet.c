9c339d868d30f20455fb157b6fa0f6200ec69db7
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-09 17:47:07 -0700

    c programming

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - void print_alphabet(void)
+ *
+ * Return: always return 0
+ */
+void print_alphabet(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'Z'; alpha++)
+_putchar(alpha);
+_putcha('\n');
+}
