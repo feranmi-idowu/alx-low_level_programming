20aad6cdadf9de54f0229d10e75b613f80dd7f70
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-15 01:00:15 -0700

    array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	int length;
+	length = 0;
+	while (*s++)
+	{
+		length++;
+	}
