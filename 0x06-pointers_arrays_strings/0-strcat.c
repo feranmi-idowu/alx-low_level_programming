a895aa576af8e1ead0041ec30b9af21d33e8439e
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-17 16:44:03 -0700

    task 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int c, c2;
-	c = 0;
-	while (dest[c])
-		c++;
-	for (c2 = 0; src[c2]; c++)
-	dest[c++] = src[c2];
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
 	return (dest);
 }
