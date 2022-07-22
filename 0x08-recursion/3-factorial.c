422988303aff075535899a87aa8f5650ee1bb93b
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-22 14:33:29 -0700

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -5,11 +5,12 @@
  * @s: input
  * Return: Always 0
  */
-
-int _strlen_recursion(char *s)
+int factorial(int n)
 {
-	if (*s == 0)
-		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
+if (n < 0)
+return (-1);
+else if (n == 0)
+return (1);
+else
+return (n * factorial(n - 1));
 }
