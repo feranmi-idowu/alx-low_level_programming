364e3bc3c5af0972d5865357be7a464acf2b1e82
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-22 17:23:07 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,13 +5,6 @@ int prime_checker(int n, int i);
  * @n: input to check
  * Return: Always 0
  */
-
-/**
- * prime_checker - checks for prime
- * @n: input to check
- * @i: n / 2, then passes to i - 1, checks if greater than 0
- * Return: prime check
- */
 int is_prime_number(int n)
 {
 if (n <= 1)
@@ -20,6 +13,13 @@ else if (prime_checker(n, n / 2) > 0)
 return (1);
 return (0);
 }
+
+/**
+ * prime_checker - checks for prime
+ * @n: input to check
+ * @i: n / 2, then passes to i - 1, checks if greater than 0
+ * Return: prime check
+ */
 int prime_checker(int n, int i)
 {
 if (i == 1)
