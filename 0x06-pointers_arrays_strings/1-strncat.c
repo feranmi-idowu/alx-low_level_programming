3ee82fa20cf6f75e04965686bc3a811dfb0a5bf3
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-24 08:40:23 -0700

    pointer

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,8 @@
-#include "main.h"
-
 /**
  * _strncat - a function that concatenates two strings
  * @dest:copy to
  * @src: copy from
  * @n: n
- * 
  * Return: Always 0
  */
 char *_strncat(char *dest, char *src, int n)
