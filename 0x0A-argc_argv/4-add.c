f73ee6e3cd2f38f900d4f4bc2506e2b18240f936
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-29 08:17:32 -0700

    task--4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * checker - checks for valid input
+ * @argc: argument count
+ * @i: counter for argv[]
+ * @j: counter for argv[][]
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+int sum = 0, i;
+
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+int b;
+char *str;
+
+str = argv[i];
+for (b = 0; str[b] != '\0'; b++)
+{
+if (str[b] < 48 || str[b] > 57)
+{
+printf("Error\n");
+return(0);
+}
+}
+}
+}
+for (i = 1; i < argc; i++)
+{
+sum += atoi(argv[i]);
+}
+printf("%d\n", sum);
+return(0);
+}
