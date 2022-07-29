9e08e1efdf6e6412c044a166bc79417670863253
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-07-29 08:51:17 -0700

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,6 +5,7 @@
  *  main - multiplies two numbers and prints the result
  *  @argc: argument count must be exactly two numbers
  *  @argv: argument vector
+ *
  *  Return: 0 on success, 1 on failure
  */
 int main(int argc, char *argv[])
