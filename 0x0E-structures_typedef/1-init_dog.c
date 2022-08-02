593473aacaf1eae06225203dcd1e1ac7f18680af
idowu oluwaferanmi <idowuferanmi4@gmail.com>
2022-08-02 08:14:17 -0700

    task1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  *@age: second member
  *@owner: third member
  *
- *Return: 0
+ *Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
@@ -19,3 +19,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->age = age;
 	d->owner = owner;
 }
+
